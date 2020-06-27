420b08e799d0971cd14a0171fff11d0ba54453ea
Milo Webster <milo.webster@gmail.com>
2020-06-27 20:31:44 +0000

    Updated baseline fork w/ GCS download commit; updated local run script

--- components/train/run_local.sh
+++ components/train/run_local.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-python3 src/voxceleb_trainer/trainSpeakerNet.py --data-bucket=voxsrc-2020-voxceleb-v4 --test_list=vox1_test_list_small.txt --train_list=vox2_train_list_small.txt --test_path=voxceleb1-small-wav.tar.gz --train_path=voxceleb2-small-m4a.tar.gz
+# @TODO run this in the components container the same was as defined in the component.yaml
+python3 src/voxceleb_trainer/trainSpeakerNet.py --data-bucket=voxsrc-2020-voxceleb-v4 --test_list=vox1_test_list_small.txt --train_list=vox2_train_list_small.txt --test_path=voxceleb1-small-wav.tar.gz --train_path=voxceleb2-small-m4a.tar.gz --skip-data-fetch=True --batch_size=30 --max_epoch=1
+
