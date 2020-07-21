fb145fbf5bf8b42317aa2b4adf4e97a68cf10cce
Milo Webster <milo.webster@gmail.com>
2020-07-21 21:41:05 +0000

    Added basic wandb integration; renamed reset training flag

--- components/train/src/SpeakerNet.py
+++ components/train/src/SpeakerNet.py
@@ -16,6 +16,8 @@ from loss.softmax import SoftmaxLoss
 from loss.protoloss import ProtoLoss
 from loss.pairwise import PairwiseLoss
 
+import wandb
+
 class SpeakerNet(nn.Module):
 
     def __init__(self, device, max_frames, lr = 0.0001, margin = 1, scale = 1, hard_rank = 0, hard_prob = 0, model="alexnet50", nOut = 512, nSpeakers = 1000, optimizer = 'adam', encoder_type = 'SAP', normalize = True, trainfunc='contrastive', **kwargs):
