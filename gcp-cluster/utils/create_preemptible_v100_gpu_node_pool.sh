d4fee87d1668800dde608b498cdebb1000c7f48e
Milo Webster <milo.webster@gmail.com>
2020-07-23 21:32:21 +0000

    Fixed metric learning and wandb issues for kubeflow/cluster training

--- gcp-cluster/utils/create_preemptible_v100_gpu_node_pool.sh
+++ gcp-cluster/utils/create_preemptible_v100_gpu_node_pool.sh
@@ -6,12 +6,13 @@ if [ -n "$1" ]; then
   gcloud container node-pools create $1 \
     --cluster=kf-train-6 \
     --enable-autoscaling --max-nodes=2 --min-nodes=0 \
-    --disk-size=800GB \
+    --disk-size=400GB \
     --num-nodes=0 \
     --preemptible \
     --machine-type=n1-standard-8 \
     --node-taints=preemptible=true:NoSchedule \
-    --service-account=$SA
+    --service-account=$SA \
+    --accelerator=type=nvidia-tesla-v100,count=1
 else
   echo "Error: Didn't provide node pool name"
 fi
